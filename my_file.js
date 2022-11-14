8c655b376e87fb086b5b8cb5ab5ccced99bdafd1
cc-aeya <aaminah.x@icloud.com>
2022-11-14 18:36:11 +0000

    listed finances

--- my_file.js
+++ my_file.js
@@ -5,5 +5,16 @@ accumalate amount inside array
 
 */
 
+console.log('Financial Analysis');
+
 var totalMonths = finances.length;
 console.log(totalMonths);
+
+
+    for (var i = 0; i < finances.length; i++) {
+        sum += finances[i][1];
+        
+        console.log(finances);
+    }
+
+
