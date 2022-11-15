aa2192f274141c12203a342963451ad16da25287
cc-aeya <aaminah.x@icloud.com>
2022-11-15 09:54:09 +0000

    edited global variables

--- my_file.js
+++ my_file.js
@@ -1,18 +1,19 @@
-/*
+/* PSEUDO Code
 total no of months is length of array
 for loop array 
 accumalate amount inside array using nested for loop
 
 */
 
+/* Heading */
 console.log('Financial Analysis');
 
-var totalMonths = finances.length;
-    //console.log(totalMonths);
-var sum = 0;
-var grandTotal = 0;
+/* Gloobal Variables */
+var totalMonths = finances.length; /*console.log(totalMonths);*/
+var netTotal = 0;
+var monthlyChange = 0;
 var avChange = 0;
-var
-
+var grProfit = 0;
+var grLoss = 0; 
 
 
