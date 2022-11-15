2dfa5bae9b2412d286ab3ae1974b18fd4aa6b51c
cc-aeya <aaminah.x@icloud.com>
2022-11-15 19:09:07 +0000

    worked avg by dividing netTotal

--- my_file.js
+++ my_file.js
@@ -10,7 +10,7 @@ console.log('Financial Analysis');
 /* Gloobal Variables */
 var totalMonths = finances.length; //no. of months (86)
 var netTotal = 0;
-var monthlyChange = 0;
+var monthlyAvg = 0;
 var avChange = 0;
 var grProfit = 0;
 var grLoss = 0; 
@@ -21,14 +21,12 @@ for (i = 0; i < finances.length; i++) {
     var figures = finances[i][1]; 
     netTotal += finances [i][1] //sum of all figures
 
-    //monthly change in profits
-    for (j = 1; j < finances[i].length; j++) { //j starts from 2nd sub-array(feb10)
-       monthlyChange = finances[j][1] - finances[i][1]
-       console.log(monthlyChange);
+    //monthly average in profits
+    avChange = netTotal/totalMonths
+    console.log(avChange);
+
+       
     }
-}
-
-
 
 
 //Ouput
