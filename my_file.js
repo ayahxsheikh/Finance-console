4e1b61076a122e602499640fd3c11bcbc899a2ae
cc-aeya <aaminah.x@icloud.com>
2022-11-15 09:14:46 +0000

    psudo code, declaring global variables

--- my_file.js
+++ my_file.js
@@ -1,20 +1,18 @@
 /*
 total no of months is length of array
 for loop array 
-accumalate amount inside array
+accumalate amount inside array using nested for loop
 
 */
 
 console.log('Financial Analysis');
 
 var totalMonths = finances.length;
-console.log(totalMonths);
+    //console.log(totalMonths);
+var sum = 0;
+var grandTotal = 0;
+var avChange = 0;
+var
 
-
-    for (var i = 0; i < finances.length; i++) {
-        sum += finances[i][1];
-        
-        console.log(finances);
-    }
 
 
