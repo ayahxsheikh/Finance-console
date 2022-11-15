c259417d607646471944e8c668ab4e28e1a09b94
cc-aeya <aaminah.x@icloud.com>
2022-11-15 11:54:56 +0000

    value of j = 1, monthly avg

--- my_file.js
+++ my_file.js
@@ -8,7 +8,7 @@ nested loop for average and use j[i][1]
 console.log('Financial Analysis');
 
 /* Gloobal Variables */
-var totalMonths = finances.length; 
+var totalMonths = finances.length; //no. of months (86)
 var netTotal = 0;
 var monthlyChange = 0;
 var avChange = 0;
@@ -18,12 +18,13 @@ var grLoss = 0;
 //totalMonths
 
 for (i = 0; i < finances.length; i++) {
-    var figures = finances[i][1]; //no. of months
-    netTotal += finances [i][1] //netTotal
+    var figures = finances[i][1]; 
+    netTotal += finances [i][1] //sum of all figures
 
     //monthly change in profits
     for (j = 1; j < finances[i].length; j++) { //j starts from 2nd sub-array(feb10)
-        monthlyChange
+       monthlyChange = finances[j][1] - finances[i][1]
+       console.log(monthlyChange);
     }
 }
 
