57dd755db1f474a7adfb26348da3008bfb8f9610
cc-aeya <aaminah.x@icloud.com>
2022-11-16 13:08:58 +0000

    added vars for loos/profit  months

--- my_file.js
+++ my_file.js
@@ -9,25 +9,35 @@ var monthlyAvg = 0;
 var monthlySum = 0;
 var totalAvg = 0;
 var grProfit = 0;
+var grMonth = '';
 var grLoss = 0; 
+var lossMonth = '';
 
 //totalMonths
 
-for (i = 0; i < finances.length; i++) {
+for (var i = 0; i < finances.length; i++) {
     netTotal += finances [i][1] //sum of all figures
 
     //monthly average in profits
-     for (j = 1; j < finances[i].length; j++) {
+     for (var j = 1; j < finances[i].length; j++) {
             monthlySum += finances[j][1] - finances[i][1]; 
             monthlyAvg = netTotal / finances.length -1;
             totalAvg = monthlyAvg.toFixed(2); 
             //used montlySum to assign totalAvg to 2 decimal places.
         }
     }
+    //The greatest increase in profits (date and amount) over the entire period
+        var profitMonth = '';
+        var grAmount;
+        for (i = 0; i < finances.length; i++) {
+            profitMonth = finances[i][0];
+            grAmount = finances[i][1];
 
-    if (((finances[j][1]) - (finances[i][1])) > grProfit) {
-        grProfit = ((finances[j][1]) - (finances[i][1]));
-    } 
+            if(grProfit <= grAmount){
+
+            }
+
+     } 
 
 
 //Ouput
